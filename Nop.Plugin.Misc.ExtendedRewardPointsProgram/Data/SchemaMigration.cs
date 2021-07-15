296a79175874b5ce5f5bb51ae418692a3c887689
BS-427\BS427 <Nazmul.Hasan@brainstation23.com>
2021-07-15 21:22:31 +0600

    Init Plugin

---
+++ Nop.Plugin.Misc.ExtendedRewardPointsProgram/Data/SchemaMigration.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+using Nop.Data.Migrations;
+using Nop.Plugin.Misc.ExtendedRewardPointsProgram.Domain;
+
+namespace Nop.Plugin.Misc.ExtendedRewardPointsProgram.Data
+{
+    [SkipMigrationOnUpdate]
+    [NopMigration("2020/02/03 09:30:17:6455422", "Misc.ExtendedRewardPointsProgram base schema")]
+    public class SchemaMigration : AutoReversingMigration
+    {
+        protected IMigrationManager _migrationManager;
+
+        public SchemaMigration(IMigrationManager migrationManager)
+        {
+            _migrationManager = migrationManager;
+        }
+
+        public override void Up()
+        {
+            _migrationManager.BuildTable<RewardPointsOnDateSettings>(Create);
+        }
+    }
+}
---
+++ Nop.Plugin.Misc.ExtendedRewardPointsProgram/Infrastructure/DependencyRegistrar.cs
@@ -0,0 +1,48 @@
+﻿using Autofac;
+using Autofac.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Nop.Core.Configuration;
+using Nop.Core.Infrastructure;
+using Nop.Core.Infrastructure.DependencyManagement;
+using Nop.Data;
+using Nop.Plugin.Misc.ExtendedRewardPointsProgram.Domain;
+using Nop.Plugin.Misc.ExtendedRewardPointsProgram.Services;
+using Nop.Services.Configuration;
+
+namespace Nop.Plugin.Misc.ExtendedRewardPointsProgram.Infrastructure
+{
+    /// <summary>
+    /// Represents dependency registrar
+    /// </summary>
+    public class DependencyRegistrar : IDependencyRegistrar
+    {
+        /// <summary>
+        /// Register services and interfaces
+        /// </summary>
+        /// <param name="services">Collection of service descriptors</param>
+        /// <param name="typeFinder">Type finder</param>
+        /// <param name="appSettings">App settings</param>
+        public void Register(IServiceCollection services, ITypeFinder typeFinder, AppSettings appSettings)
+        {
+            //register custom services
+            services.AddScoped<IRewardPointsOnDateSettingsService, RewardPointsOnDateSettingsService>();
+        }
+
+        /// <summary>
+        /// Register services and interfaces
+        /// </summary>
+        /// <param name="builder">Container builder</param>
+        /// <param name="typeFinder">Type finder</param>
+        /// <param name="config">Config</param>
+        //public virtual void Register(ContainerBuilder builder, ITypeFinder typeFinder, NopConfig config)
+        //{
+        //    //register override services
+        //    builder.RegisterType<ExtendedRewardPointsProgramSettingService>().As<ISettingService>().InstancePerLifetimeScope();
+        //}
+
+        /// <summary>
+        /// Order of this dependency registrar implementation
+        /// </summary>
+        public int Order => 1;
+    }
+}
